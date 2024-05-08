1a62e60800958d74220d3e55d49e1f1abe69e3f9
yangsufang <yangsufang@kilox.cn>
2024-05-08 16:59:15 +0800

    test_class

--- tests/test01.py
+++
@@ -1,8 +0,0 @@
-import time
-from selenium import webdriver
-
-def test_yoyo_01(browser):
-    browser.get('https://www.cnblogs.com/yoyoketang/')
-    time.sleep(3)
-    t = browser.title
-    assert t == '上海-悠悠'
